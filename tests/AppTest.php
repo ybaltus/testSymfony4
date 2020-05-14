38d243db64cd0c5e58384fe7f6990c7463ec7336
Yannis <yannis.baltus@outlook.com>
2020-05-14 20:38:59 +0200

    Initial commit

---
+++ tests/AppTest.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+
+class AppTest extends TestCase
+{
+    public function testTestAreWorking(){
+        $this->assertEquals(4, 2+3);
+    }
+
+}
